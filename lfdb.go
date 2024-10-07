903c2c5e384407d8977ec18198b587e9340ad990
nirione <swierczjakub@yahoo.com>
2024-10-08 01:47:56 +0200

    filmInfo

--- lfdb.go
+++ lfdb.go
@@ -4,16 +4,60 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"html/template"
 )
 
+type Person struct {
+	FirstName 	string
+	LastName	string
+	DOB			string
+	Occupation	string
+}
+
+type Film struct {
+	FilmID		int
+	Title		string
+	Year		int
+	Director	Person
+	Genre		string
+	Rating 		float32
+	Series		bool
+}
+
+var film1 Film
+var person1 Person
+var tpl *template.Template
+
 func main() {
+	person1 = Person{
+		FirstName: "Sergio",
+		LastName: "Leone",
+		DOB: "03.01.1929",
+		Occupation: "Director",
+	}
+
+	film1 = Film{
+		FilmID: 1,
+		Title: "Once Upon a Time in The West",
+		Year: 1968,
+		Director: person1,
+		Genre: "Western",
+		Rating: 0.9,
+		Series: false,
+	}
+
+	tpl, _ = tpl.ParseGlob("webpage/*.html")
+
 	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":8080", nil)
-	
+	http.HandleFunc("/film", filmHandler)
+	http.ListenAndServe(":8080", nil)	
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	dat, _ := os.ReadFile("./test")
 	fmt.Fprintf(w, string(dat))
+}
 
+func filmHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "filmInfo.html", film1)
 }
